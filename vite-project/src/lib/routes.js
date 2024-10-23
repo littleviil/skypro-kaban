5540d2cac2823cd760984a81d232ac2a261e3cc4
littleviil <netlena30@gmail.com>
2024-10-23 20:36:47 +0300

    +route and PrivateRoute

---
+++ vite-project/src/lib/routes.js
@@ -0,0 +1,9 @@
+export const routesPages={
+    MAIN: "/",
+    SIGNIN: "/signin",
+    CARD: "/card/:userId",
+    NewCard: "/newcard",
+    REGISTER: "/register",
+    EXIT: "/exit",
+    NOT_FOUND: "*"
+};
